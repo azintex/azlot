b569a007c220e8650e896be2a047402863364278
Haji Uzdenov <h.uzdenov@azintex.com>
2019-10-30 15:25:57 +0400

    New 5/36 generator

---
+++ 536.py
@@ -0,0 +1,13 @@
+fh = open('res.txt', 'w')
+if int(input("5/36 : ")) < 15:
+    print("Wrong value")
+elif > 170:
+    print("Wrong value")
+# for n in range(5,37):
+#     for m in range(4,36):
+#         for l in range(3,35):
+#             for k in range(2,34):
+#                 for j in range(1,33):
+#                     if j < k and k < l and l < m and m < n:
+#                         t = (j,k,l,m,n)
+fh.close()
