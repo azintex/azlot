2474d1185806d0d830371d656c0c9480ad5d8a6f
Haji Uzdenov <h.uzdenov@azintex.com>
2020-08-11 16:18:03 +0400

    Minor changes

--- 536.py
+++ 536.py
@@ -1,24 +1,33 @@
-fh = open('result.txt', 'w')
+result = open('result.txt', 'w')
 
-inpt = int()
+inputedSum = int(input('Type sum : '))
 
-def showSumPrompt():
-    global inpt
-    inpt = int(input("5/36 : "))
-    if inpt < 15 or inpt > 170:
-        print("Value must be more than 15 and less than 170")
-        showSumPrompt()
+if(inputedSum < 15 or inputedSum > 170):
+    print('Sum must be more than 14 and less 171')
 
-showSumPrompt()
 
-for n in range(5,37):
-    for m in range(4,36):
-        for l in range(3,35):
-            for k in range(2,34):
-                for j in range(1,33):
-                    if j < k and k < l and l < m and m < n:
-                        t = (j,k,l,m,n)
-                        if sum(t) == inpt:
-                            fh.write("%s\r" % str(t))
+# def showPrompt():
+#     return int(input('Type sum more than 15 and less 170 : '))
 
-fh.close()
+
+# def getSum():
+#     #inputedSum = int(input('Sum must be more than 15 and less 170 : '))
+#     value = showPrompt()
+#     if value < 15 or value > 170:
+#         print('Not acceptible sum value, type again')
+#         showPrompt()
+#     return value
+
+# def calcSum(value):
+#     for n in range(5,37):
+#         for m in range(4,36):
+#             for l in range(3,35):
+#                 for k in range(2,34):
+#                     for j in range(1,33):
+#                         if j < k and k < l and l < m and m < n:
+#                             t = (j,k,l,m,n)
+#                             if sum(t) == value:
+#                                 result.write("%s\r" % str(t))
+#                                 result.close()
+
+# calcSum(getSum())
